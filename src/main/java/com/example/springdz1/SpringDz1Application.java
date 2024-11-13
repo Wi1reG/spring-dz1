730675621d5e2cbe2d28f595ddbcfaa475ac1200
Wi1reG <anikin-andrey1@mail.ru>
2024-11-13 18:37:13 +0300

    для пуша

---
+++ src/main/java/com/example/springdz1/SpringDz1Application.java
@@ -0,0 +1,13 @@
+package com.example.springdz1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDz1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDz1Application.class, args);
+	}
+
+}
